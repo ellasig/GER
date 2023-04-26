ff3ae28c5003601c49b895beb527c901f997a67b
Aapelii <aapeli.vaisanen@gmail.com>
2023-04-26 19:48:30 +0300

    ratingAction.js added

--- server/routes/ratingAction.js
+++ server/routes/ratingAction.js
@@ -5,7 +5,17 @@ const {stringify} = require('nodemon/lib/utils');
 const router = express.Router();
 
 router.put('/rateContent', function(req, res) {
+    let sql = "INSERT INTO user_content (user_id, content_id, user_raiting) VALUES (?, ?, ?)";
 
+    conn.query(sql, [req.body.user_id, req.body.content_id, req.body.user_rating], function (err, result) {
+        if (err) {
+            res.status(400).send({
+                text: "Error adding content"
+            });
+        } else {
+            res.status(200).send("POST successful");
+        }
+    });
 });
 
 
